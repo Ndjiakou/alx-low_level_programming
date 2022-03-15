d5071249e6a453261b805591f4561f8c7fd52ce7
Ndjiakou <Pndjiakoumaiva@gmail.com>
2022-03-14 17:48:59 -0700

    my 1 astk commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /**
  * main-entry point
  *
- * return:0 (sucess)
+ * return:Always 0 (Success)
  */
 int main(void)
 {
@@ -19,7 +19,7 @@ else if (n % 10 == 0)
 {
 	printf("Last digit of %i is %i and is 0\n", n, n % 10);
 }
-else if (n % 10 < 6 && n% 10 != 0) 
+else if (n % 10 < 6 && n % 10 != 0) 
 {
 	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 }
