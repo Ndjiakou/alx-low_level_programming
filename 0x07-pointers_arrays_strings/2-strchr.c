ee085164f5c722af644dc2df951b0540e38f72a5
Ndjiakou <Pndjiakoumaiva@gmail.com>
2022-03-28 17:46:30 -0700

    2 task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strchr - Locates a character in a string
+ *@s: String
+ *@c: Character to search
+ *Return: Pointer to the first occurrence of the character c in the string s
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i] >= '\0'  ; i++)
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+}
+return ('\0');
+}
