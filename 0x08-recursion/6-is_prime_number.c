bd2b7e11763645d47a5bf00682c86f8629ef5c26
Ndjiakou <Pndjiakoumaiva@gmail.com>
2022-03-30 19:24:24 -0700

    6 task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -21,13 +21,11 @@ return (evaluate_num(num, iterator + 1));
 }
 return (0);
 }
-
 /**
- *  *is_prime_number - evaluate prime or not
- *   *@num: number
- *    *Return: return 1 prime - return 0 otherwise
- *     */
-
+ *is_prime_number - evaluate prime or not
+ *@num: number
+ *Return: return 1 prime - return 0 otherwise
+ */
 int is_prime_number(int num)
 {
 int iterator;
