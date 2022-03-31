f29e3377e01185add74d2ebe503702eb385781f5
Ndjiakou <Pndjiakoumaiva@gmail.com>
2022-03-30 17:58:47 -0700

    2 task

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_strlen_recursion - length of a string
+ *@s: pointer block of memory to fill
+ *Return: strlen_recursion
+ */
+int _strlen_recursion(char *s)
+{
+int n = 0;
+if(*s == '\0')
+{
+return(n);
+}
+else 
+{
+n += _strlen_recursion(s + 1) + 1;
+return(n);
+}
+}
